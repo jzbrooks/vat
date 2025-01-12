21a44a84cd888552b871a55061ad73405cf95a34
Justin Brooks <justin@jzbrooks.com>
2025-01-12 15:46:05 -0500

    Add build tasks for other systems

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.internal.extensions.stdlib.capitalized
 import java.io.PrintWriter
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -17,28 +18,28 @@ repositories {
 
 val r8: Configuration by configurations.creating
 
+val targets = mapOf(
+    "macos" to listOf("arm64"),
+    "windows" to listOf("x64", "arm64"),
+    "linux" to listOf("x64", "arm64"),
+)
+
+for ((os, architectures) in targets) {
+    for (arch in architectures) {
+        configurations.create("$os${arch.capitalized()}RuntimeOnly")
+    }
+}
+
 dependencies {
-    val osName = System.getProperty("os.name")
-    val targetOs =
-        when {
-            osName == "Mac OS X" -> "macos"
-            osName.startsWith("Win") -> "windows"
-            osName.startsWith("Linux") -> "linux"
-            else -> error("Unsupported OS: $osName")
-        }
-
-    val osArch = System.getProperty("os.arch")
-    val targetArch =
-        when (osArch) {
-            "x86_64", "amd64" -> "x64"
-            "aarch64" -> "arm64"
-            else -> error("Unsupported arch: $osArch")
-        }
-
-    val target = "$targetOs-$targetArch"
-
     implementation("org.jetbrains.skiko:skiko:0.8.18")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-$target:0.8.18")
+    for ((os, architectures) in targets) {
+        for (arch in architectures) {
+            configurations.named("$os${arch.capitalized()}RuntimeOnly").configure {
+                this("org.jetbrains.skiko:skiko-awt-runtime-$os-$arch:0.8.18")
+            }
+        }
+    }
+
     implementation("com.jzbrooks:vgo:3.0.0")
     implementation("com.jzbrooks:vgo-core:3.0.0")
 
@@ -56,110 +57,6 @@ kotlin.sourceSets
 tasks {
     test {
         useJUnitPlatform()
-    }
-
-    compileKotlin {
-        dependsOn("generateConstants")
-    }
-
-    named("runKtlintCheckOverMainSourceSet") {
-        dependsOn("generateConstants")
-    }
-
-    named("runKtlintFormatOverMainSourceSet") {
-        dependsOn("generateConstants")
-    }
-
-    jar {
-        dependsOn(configurations.runtimeClasspath)
-        manifest {
-            attributes["Main-Class"] = "com.jzbrooks.vat.MainKt"
-            attributes["Bundle-Version"] = version
-        }
-
-        val sourceClasses =
-            sourceSets.main
-                .get()
-                .output.classesDirs
-        inputs.files(sourceClasses)
-        destinationDirectory.set(layout.buildDirectory.dir("libs/debug"))
-
-        doFirst {
-            from(files(sourceClasses))
-            from(
-                configurations.runtimeClasspath
-                    .get()
-                    .asFileTree.files
-                    .map(::zipTree),
-            )
-
-            exclude(
-                "**/*.kotlin_metadata",
-                "**/*.kotlin_module",
-                "**/*.kotlin_builtins",
-                "**/module-info.class",
-                "META-INF/maven/**",
-                "META-INF/*.version",
-                "META-INF/LICENSE*",
-                "META-INF/LGPL2.1",
-                "META-INF/DEPENDENCIES",
-                "META-INF/AL2.0",
-                "META-INF/BCKEY.DSA",
-                "META-INF/BC2048KE.DSA",
-                "META-INF/BCKEY.SF",
-                "META-INF/BC2048KE.SF",
-                "**/NOTICE*",
-                "javax/activation/**",
-                "xsd/catalog.xml",
-            )
-        }
-    }
-
-    val optimize by registering(JavaExec::class) {
-        description = "Runs r8 on the jar application."
-        group = "build"
-
-        inputs.file("build/libs/debug/vat-$version.jar")
-        outputs.file("build/libs/vat.jar")
-
-        val javaHome = System.getProperty("java.home")
-
-        classpath(r8)
-        mainClass = "com.android.tools.r8.R8"
-
-        args(
-            "--release",
-            "--classfile",
-            "--lib",
-            javaHome,
-            "--output",
-            "build/libs/vat.jar",
-            "--pg-conf",
-            "optimize.pro",
-            "build/libs/debug/vat-$version.jar",
-        )
-
-        dependsOn(getByName("jar"))
-    }
-
-    val binaryFileProp = layout.buildDirectory.file("libs/vat")
-    val binary by registering {
-        description = "Prepends shell script in the jar to improve CLI"
-        group = "build"
-
-        dependsOn(optimize)
-
-        inputs.file("build/libs/vat.jar")
-        outputs.file(binaryFileProp)
-
-        doLast {
-            val binaryFile = binaryFileProp.get().asFile
-            binaryFile.parentFile.mkdirs()
-            binaryFile.delete()
-            binaryFile.appendText("#!/bin/sh\n\nexec java \$JAVA_OPTS -jar \$0 \"\$@\"\n\n")
-            file("build/libs/vat.jar").inputStream().use { binaryFile.appendBytes(it.readBytes()) }
-            binaryFile.setExecutable(true, false)
-        }
     }
 
     val generateConstants by registering {
@@ -198,5 +95,147 @@ tasks {
                 output.write(buildConstantsClass)
             }
         }
+    }
+
+    named("runKtlintCheckOverMainSourceSet") {
+        dependsOn(generateConstants)
+    }
+
+    named("runKtlintFormatOverMainSourceSet") {
+        dependsOn(generateConstants)
+    }
+
+    compileKotlin {
+        dependsOn(generateConstants)
+    }
+
+    for ((os, architectures) in targets) {
+        for (arch in architectures) {
+            register<Jar>("$os${arch.capitalized()}Jar") {
+                dependsOn(configurations.runtimeClasspath)
+
+                manifest {
+                    attributes["Main-Class"] = "com.jzbrooks.vat.MainKt"
+                    attributes["Bundle-Version"] = version
+                }
+
+                val sourceClasses = sourceSets.main.get().output.classesDirs
+
+                inputs.files(sourceClasses)
+
+                archiveBaseName.set("vat-$os-$arch")
+                destinationDirectory.set(layout.buildDirectory.dir("libs/debug"))
+
+                from(files(sourceClasses))
+                from(
+                    configurations.runtimeClasspath
+                        .get()
+                        .asFileTree.files
+                        .map(::zipTree),
+                )
+
+                exclude(
+                    "**/*.kotlin_metadata",
+                    "**/*.kotlin_module",
+                    "**/*.kotlin_builtins",
+                    "**/module-info.class",
+                    "META-INF/maven/**",
+                    "META-INF/*.version",
+                    "META-INF/LICENSE*",
+                    "META-INF/LGPL2.1",
+                    "META-INF/DEPENDENCIES",
+                    "META-INF/AL2.0",
+                    "META-INF/BCKEY.DSA",
+                    "META-INF/BC2048KE.DSA",
+                    "META-INF/BCKEY.SF",
+                    "META-INF/BC2048KE.SF",
+                    "**/NOTICE*",
+                    "javax/activation/**",
+                    "xsd/catalog.xml",
+                )
+            }
+
+            register<JavaExec>("$os${arch.capitalized()}Optimize") {
+                description = "Runs r8 on the jar application."
+                group = "build"
+
+                inputs.file("build/libs/debug/vat-$os-$arch-$version.jar")
+                outputs.file("build/libs/vat-$os-$arch-$version.jar")
+
+                val javaHome = System.getProperty("java.home")
+
+                classpath(r8)
+                mainClass = "com.android.tools.r8.R8"
+
+                args(
+                    "--release",
+                    "--classfile",
+                    "--lib",
+                    javaHome,
+                    "--output",
+                    "build/libs/vat-$os-$arch-$version.jar",
+                    "--pg-conf",
+                    "optimize.pro",
+                    "build/libs/debug/vat-$os-$arch-$version.jar",
+                )
+
+                dependsOn("$os${arch.capitalized()}Jar")
+            }
+
+            val binaryFileProp = layout.buildDirectory.file("libs/vat-$os-$arch")
+            register("$os${arch.capitalized()}Binary") {
+                description = "Prepends shell script in the jar to improve CLI"
+                group = "build"
+
+                dependsOn("$os${arch.capitalized()}Optimize")
+
+                inputs.file("build/libs/vat-$os-$arch-$version.jar")
+                outputs.file(binaryFileProp)
+
+                doLast {
+                    val binaryFile = binaryFileProp.get().asFile
+                    binaryFile.parentFile.mkdirs()
+                    binaryFile.delete()
+                    binaryFile.appendText("#!/bin/sh\n\nexec java \$JAVA_OPTS -jar \$0 \"\$@\"\n\n")
+                    file("build/libs/vat-$os-$arch-$version.jar").inputStream().use { binaryFile.appendBytes(it.readBytes()) }
+                    binaryFile.setExecutable(true, false)
+                }
+            }
+        }
+    }
+
+    val osName = System.getProperty("os.name")
+    val targetOs =
+        when {
+            osName == "Mac OS X" -> "macos"
+            osName.startsWith("Win") -> "windows"
+            osName.startsWith("Linux") -> "linux"
+            else -> error("Unsupported OS: $osName")
+        }
+
+    val osArch = System.getProperty("os.arch")
+    val targetArch =
+        when (osArch) {
+            "x86_64", "amd64" -> "x64"
+            "aarch64" -> "arm64"
+            else -> error("Unsupported arch: $osArch")
+        }
+
+    jar {
+        description = "Runs r8 on the jar application for the current system ($targetOs-$targetArch)."
+        group = "build"
+        dependsOn("$targetOs${targetArch.capitalized()}Jar")
+    }
+
+    val optimize by registering {
+        description = "Runs r8 on the jar application for the current system ($targetOs-$targetArch)."
+        group = "build"
+        dependsOn("$targetOs${targetArch.capitalized()}Optimize")
+    }
+
+    val binary by registering {
+        description = "Prepends shell script in the jar to improve CLI for the current system ($targetOs-$targetArch)"
+        group = "build"
+        dependsOn("$targetOs${targetArch.capitalized()}Binary")
     }
 }
