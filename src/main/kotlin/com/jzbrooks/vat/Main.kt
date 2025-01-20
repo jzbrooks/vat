b35d7edf5e74974775cecd71b8eb780e7680603e
Justin Brooks <justin@jzbrooks.com>
2025-01-19 22:55:16 -0500

    Also accept hex colors prefixed by #

--- src/main/kotlin/com/jzbrooks/vat/Main.kt
+++ src/main/kotlin/com/jzbrooks/vat/Main.kt
@@ -54,7 +54,7 @@ fun main(args: Array<String>) {
     } ?: 1f
 
     val backgroundColor = argReader.readOption("background-color")?.let {
-        val rgba = it.removePrefix("0x").chunked(2).mapNotNull { it.toIntOrNull(16) }
+        val rgba = it.removePrefix("0x").removePrefix("#").chunked(2).mapNotNull { it.toIntOrNull(16) }
         if (rgba.size == 4) {
             // The RGBA color must be re-packed as ARGB for skia, per its color packing rules
             (rgba[3] shl 24) or (rgba[0] shl 16) or (rgba[1] shl 8) or rgba[2]
